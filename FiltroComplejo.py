5b73136fe6afe4b438a57ed4505c00a561294a25
Milagros Aylen Jacinto <mil.aylen.j@gmail.com>
2023-05-03 21:54:44 -0300

    Arregla filtro de contraste y crea ejecutable con el cambio

--- FiltroComplejo.py
+++ FiltroComplejo.py
@@ -10,6 +10,6 @@ class FiltroComplejo(Filtro):
                         "-AJ_ROJO-" : [0,0,1,0,1,0,1,0, 0,0,0,value],
                         "-AJ_VERDE-": [0,0,1,0,1,0,1,0, 0,0,value,0],
                         "-AJ_AZUL-":  [0,0,1,0,1,0,1,0, 0,value,0,0],  
-                        "-CONTRASTE-": [f, 0, 0, 0, f, 0, 0, 0, f, 128*(1-f), 128*(1-f), 128*(1-f)],   
+                        "-CONTRASTE-":[0,0,f,0,f,0,f,0,0, 128*(1-f), 128*(1-f), 128*(1-f)],   
         }
         self.matriz = dict_matriz[key]
